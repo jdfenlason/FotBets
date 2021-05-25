ceb6dec278e859b453fc26e918ce137afd96b37f
Jake Fenlason <jdfenlason@gmail.com>
2021-05-25 19:01:05 +0000

    finished database schema

--- database/schema.sql
+++ database/schema.sql
@@ -6,27 +6,9 @@ drop schema "public" cascade;
 
 create schema "public";
 
-create table "public"."fixtures" (
-  "matchId"         serial,
-  "fixtureId"       integer      not null,
-  "fixtureRef"      text         not null,
-  "fixtureDate"     text         not null,
-  "fixtureTime"     integer      not null,
-  "fixtureVenue"    text         not null,
-  "fixtureStatus"   text         not null,
-  "leagueId"        text         not null,
-  "leagueName"      text         not null,
-  "teamsHomeId"     integer      not null,
-  "teamsAwayId"     integer      not null,
-  "teamsHomeName"   text         not null,
-  "teamsAwayName"   text         not null,
-  "teamsHomeLogo"   text         not null,
-  "teamsAwayLogo"   text         not null,
-  "teamsHomeWinner" boolean      not null,
-  "teamsAwayWinner" boolean      not null,
-  "createdAt"       timestamptz(6) not null default now(),
-  "updatedAt"       timestamptz(6) not null default now(),
-  primary key ("matchId")
+create table "public"."weekGames" (
+    "gamesId" serial,
+     "matches"    json
 );
 
 create table "public"."users" (
