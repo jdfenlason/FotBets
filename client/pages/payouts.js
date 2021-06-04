45d4b176677d03934ae2dc0b6c88c6b6b5a21b38
Jake Fenlason <jdfenlason@gmail.com>
2021-06-04 23:11:51 +0000

    small fix to floor

--- client/pages/payouts.js
+++ client/pages/payouts.js
@@ -1,10 +1,10 @@
 const makeBets = (stake, odds) => {
-  const profit = Math.floor(stake * odds - 1);
+  const profit = Math.floor(stake * odds - 1).toFixed(0);
   return profit;
 };
 
 const makeBetsScript = (stake, odds) => {
-  const totalPayout = stake * odds;
+  const totalPayout = Math.floor(stake * odds);
   const profit = Math.floor(stake * (odds - 1));
   return `Your total payout will be ${totalPayout} tokens, and your profit will be ${profit} tokens`;
 };
