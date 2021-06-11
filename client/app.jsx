fc916e3b00757da8900136758568bb4703cb9f42
Jake Fenlason <jdfenlason@gmail.com>
2021-06-11 18:54:27 +0000

    reverted for pr

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Main from './pages/main';
+import Home from './pages/home';
+import axios from 'axios';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -7,21 +8,65 @@ export default class App extends React.Component {
       isLoading: true,
       userName: '',
       userTokens: '',
-      userId: 2
+      userId: 2,
+      profileOn: false,
+      fixturesOn: true
     };
+    this.handleProfile = this.handleProfile.bind(this);
+    this.handleFixtures = this.handleFixtures.bind(this);
+    this.handleTokenChange = this.handleTokenChange.bind(this);
+  }
+
+  handleProfile(event) {
+    this.setState(prevState => ({
+      profileOn: true,
+      fixturesOn: false
+    }));
+  }
+
+  handleFixtures(event) {
+    this.setState(prevState => ({
+      profileOn: false,
+      fixturesOn: true
+    }));
+  }
+
+  handleTokenChange(wagerAmount) {
+    const currentTokenAmount = this.state.userTokens;
+    const changeTokenAmount = currentTokenAmount - wagerAmount;
+    this.setState({
+      userTokens: changeTokenAmount
+    });
   }
 
   componentDidMount() {
-    this.setState({
-      isLoading: false
+    const userId = {
+      userId: this.state.userId
+    };
+    axios.get('/api/user-profile', { params: userId }).then(response => {
+      this.setState({
+        userName: response.data.userName,
+        userTokens: response.data.tokenAmount,
+        isLoading: false
+      });
     });
   }
 
   render() {
+    const { userName, userTokens, profileOn, fixturesOn, fixtures } = this.state;
+    const { handleProfile, handleFixtures, handleTokenChange } = this;
     return (
       this.state.isLoading
         ? <p className='hidden'>isLoading</p>
-        : <Main />
+        : <Home userName={userName}
+             userTokens={userTokens}
+             handleProfile ={handleProfile}
+             handleFixtures = {handleFixtures}
+             profileOn ={profileOn}
+             fixturesOn = {fixturesOn}
+             fixtures = {fixtures}
+             handleTokenChange ={handleTokenChange}
+             />
     );
   }
 }
