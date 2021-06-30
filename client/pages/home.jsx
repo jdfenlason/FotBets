757b246d1809d8715c94f7f113b340cebffbb088
Jake Fenlason <jdfenlason@gmail.com>
2021-06-30 02:22:51 +0000

    fixed homepage

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { useMediaQuery } from 'react-responsive';
 const Home = props => {
+  const isNotMobile = useMediaQuery({ minWidth: 900 });
   return (
     <>
       <div className="overlay"></div>
-      <div className="home-hero-image">
+      <div className="bg-image home-bg">
           <div className="title title-row">
             <span className="large">
               <h2>
@@ -13,20 +14,20 @@ const Home = props => {
             </span>
             </div>
 
-            <div className="align message-column">
-              <h3>
+            <div className="align message-row">
+              <h3 className = {isNotMobile ? 'lg' : ''}>
                 An interactive competition against your friends to rack up the
                 most tokens!
               </h3>
           </div>
         <div className="button-container row button-row">
           <a href="#sign-in">
-            <button className="enter-button " type="submit">
+            <button className="sign-buttons " type="submit">
               Sign In
             </button>
           </a>
           <a href="#sign-up">
-            <button className="enter-button" type="button">
+            <button className="sign-buttons" type="button">
               Sign Up
             </button>
           </a>
